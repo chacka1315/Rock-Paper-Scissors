31f8205013ca22b78ad34cf18493b58b93d4ac90
chacka1315 <chacka1315@gmail.com>
2025-06-22 12:38:04 +0000

    One index.js, I have wrote the function that return the computer choice randomly.
    I did it because when we will play, we allways need
    the computer choice randomly.

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+function getComputerChoice() {
+    const choice = {
+        choice1 : "Rock",
+        choice2: "Paper",
+        choice3 : "Scissors",
+    };
+    let computerChoice = `choice${Math.floor(Math.random() *3) + 1}`;
+    return(choice[computerChoice])   
+}
 
 
-
-console.log("hello world")
